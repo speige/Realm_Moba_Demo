7f4f00059b16a33a784abd921fcee23e4da8dc97
Devin Garner <devingarner@gmail.com>
2026-08-20 12:15:35 -0600

    Initial commit

---
+++ MapScript.cs
@@ -0,0 +1,15 @@
+namespace Realm.Maps;
+
+using Realm.MapAPI;
+
+public class CustomMap : IWasmModule
+{
+    public void Initialize(IGameAPI api)
+    {
+        // test
+    }
+
+    public void Update(IGameAPI api, float delta)
+    {
+    }
+}
